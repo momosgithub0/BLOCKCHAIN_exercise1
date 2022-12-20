85e2a1187c2b42c266c059b7ca1aeb66c5969279
Mohamed Makled <if21b037@technikum-wien.at>
2022-12-20 23:03:32 +0100

    Erster Commit

---
+++ dapp.js
@@ -0,0 +1,8 @@
+// Import the web3 library
+const Web3 = require('web3');
+
+// Connect to the Ethereum blockchain
+const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY');
+
+// Display a message in the console
+console.log('Hello, World!');
